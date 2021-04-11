b1d14ffd5ab130556283fcbc3c11ef907bd1545c
ruchira010401 <82302024+ruchira010401@users.noreply.github.com>
2021-04-11 12:01:53 +0530

    Update TradeManagementApplication.java

--- trade-management/trade-management/src/main/java/com/trade/management/TradeManagementApplication.java
+++ trade-management/trade-management/src/main/java/com/trade/management/TradeManagementApplication.java
@@ -2,8 +2,10 @@ package com.trade.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TradeManagementApplication {
 
 	public static void main(String[] args) {
